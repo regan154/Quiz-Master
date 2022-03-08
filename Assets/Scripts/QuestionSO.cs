fc863b0d09d7fe7da94b2728651ec0e390569f05
Kris Regan <regan154@morrisville.edu>
2022-03-08 14:38:58 -0500

    Arrays

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,9 +6,19 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string strQuestion = "Enter new question text here";
-
+    [SerializeField] string[] strAnswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
     public string GetQuestion()
     {
         return strQuestion;
     }
+    public string GetAnswer(int index)
+    {
+        return strAnswers[index];
+    }
+    public int GetCorrectAnswerIndex()
+    {
+        return intCorrectAnswerIndex;
+    }
+
 }
